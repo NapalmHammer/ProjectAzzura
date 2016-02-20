b7234e44fedb86211defc0c92b9e3dbc79f5a70d
Will O <napalmhammer@gmail.com>
2016-02-20 14:29:17 -0500

    Added Allegro library

--- Azzura/Azzura-Core/main.cpp
+++ Azzura/Azzura-Core/main.cpp
@@ -1,6 +1,24 @@
+#include "allegro5\allegro5.h"
 #include <iostream>
 
-int main()
+int main(int argc, char** argv)
 {
+	al_init();
+
+	ALLEGRO_DISPLAY *display = nullptr;
+	al_set_app_name("Hello World from Allegro 5.1!");
+	display = al_create_display(640, 480);
+	if (display == nullptr)
+	{
+		std::cerr << "Well, something is not working..." << std::endl;
+		al_rest(5.0);
+		return EXIT_FAILURE;
+	}
+
+	al_clear_to_color(al_map_rgb(255, 255, 255));
+	al_flip_display();
+	al_rest(5.0);
 	return 0;
+
+
 }
