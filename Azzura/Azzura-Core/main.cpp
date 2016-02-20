bd8e786d80e8cfd7a36276ffa73d8528ab190b67
Will O <napalmhammer@gmail.com>
2016-02-20 18:35:52 -0500

    Created some test window code in main.cpp

--- Azzura/Azzura-Core/main.cpp
+++ Azzura/Azzura-Core/main.cpp
@@ -1,24 +1,77 @@
-#include "allegro5\allegro5.h"
 #include <iostream>
+#include "allegro5\allegro5.h"
+#include "allegro5\allegro_image.h"
+#include "allegro5\allegro_primitives.h"
 
 int main(int argc, char** argv)
 {
-	al_init();
 
-	ALLEGRO_DISPLAY *display = nullptr;
-	al_set_app_name("Hello World from Allegro 5.1!");
-	display = al_create_display(640, 480);
-	if (display == nullptr)
-	{
-		std::cerr << "Well, something is not working..." << std::endl;
-		al_rest(5.0);
-		return EXIT_FAILURE;
+	ALLEGRO_DISPLAY *display = NULL;
+	ALLEGRO_EVENT_QUEUE *event_queue = NULL;
+	//ALLEGRO_DISPLAY_MODE disp_data;
+
+	if(!al_init())	{		std::cerr << "Allegro failed to initialize!" << std::endl;
+		return 0;
 	}
 
-	al_clear_to_color(al_map_rgb(255, 255, 255));
+	/*if (!al_init_image_addon())	{		std::cerr << "Allegro Image Addon failed to initialize!" << std::endl;
+		return 0;
+	}
+
+	if (!al_init_primitives_addon())	{		std::cerr << "Allegro Primitives Addon failed to initialize!" << std::endl;
+		return 0;
+	}
+
+	al_get_display_mode(al_get_num_display_modes() - 1, &disp_data);
+
+	al_set_new_display_flags(ALLEGRO_FULLSCREEN);
+
+	display = al_create_display(disp_data.width, disp_data.height);*/
+
+	display = al_create_display(640, 480);
+
+	if (!display)
+	{
+		std::cerr << "Display failed!" << std::endl;
+		return 0;
+	}
+
+	event_queue = al_create_event_queue();
+
+	if (!event_queue)
+	{
+		std::cerr << "event Queue failed!" << std::endl;
+		al_destroy_display(display);
+		return 0;
+	}
+
+	al_set_app_name("Azzura!");
+
+	al_register_event_source(event_queue, al_get_display_event_source(display));
+
+	al_clear_to_color(al_map_rgb(0, 0, 0));
+
 	al_flip_display();
-	al_rest(5.0);
+
+	while (1)
+	{
+		ALLEGRO_EVENT ev;
+		ALLEGRO_TIMEOUT timeout;
+		al_init_timeout(&timeout, 0.06);
+
+		bool get_event = al_wait_for_event_until(event_queue, &ev, &timeout);
+
+		if (get_event && ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE)
+		{
+			break;
+		}
+
+		al_clear_to_color(al_map_rgb(0, 0, 0));
+		al_flip_display();
+	}
+
+	al_destroy_display(display);
+	al_destroy_event_queue(event_queue);
+
 	return 0;
-
-
 }
