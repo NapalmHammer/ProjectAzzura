0deca91c14a3440d5c301fb09ed1aac97de6786e
Will O <napalmhammer@gmail.com>
2016-02-22 03:57:02 -0500

    Flying pink cube

--- Azzura/Azzura-Core/main.cpp
+++ Azzura/Azzura-Core/main.cpp
@@ -1,75 +1,121 @@
 #include <iostream>
 #include "allegro5\allegro5.h"
-#include "allegro5\allegro_image.h"
-#include "allegro5\allegro_primitives.h"
+
+const float FPS = 60;
+const int SCREEN_W = 640;
+const int SCREEN_H = 480;
+const int BOUNCER_SIZE = 32;
 
 int main(int argc, char** argv)
 {
 
 	ALLEGRO_DISPLAY *display = NULL;
 	ALLEGRO_EVENT_QUEUE *event_queue = NULL;
-	//ALLEGRO_DISPLAY_MODE disp_data;
+	ALLEGRO_TIMER *timer = NULL;
+	ALLEGRO_BITMAP *bouncer = NULL;
 
-	if(!al_init())	{		std::cerr << "Allegro failed to initialize!" << std::endl;
-		return 0;
+	float bouncer_x = SCREEN_W / 2.0 - BOUNCER_SIZE / 2.0;
+	float bouncer_y = SCREEN_H / 2.0 - BOUNCER_SIZE / 2.0;
+	float bouncer_dx = -4.0, bouncer_dy = 4.0;
+	bool redraw = true;
+
+	if(!al_init())	
+	{
+		std::cerr << "Allegro failed to initialize!" << std::endl;
+		return -1;
 	}
 
-	/*if (!al_init_image_addon())	{		std::cerr << "Allegro Image Addon failed to initialize!" << std::endl;
-		return 0;
+	timer = al_create_timer(1.0 / FPS);
+	if (!timer)
+	{
+		std::cerr << "Failed to create timer!" << std::endl;
+		al_destroy_timer(timer);
+		return -1;
 	}
 
-	if (!al_init_primitives_addon())	{		std::cerr << "Allegro Primitives Addon failed to initialize!" << std::endl;
-		return 0;
-	}
-
-	al_get_display_mode(al_get_num_display_modes() - 1, &disp_data);
-
-	al_set_new_display_flags(ALLEGRO_FULLSCREEN);
-
-	display = al_create_display(disp_data.width, disp_data.height);*/
-
-	display = al_create_display(640, 480);
-
+	display = al_create_display(SCREEN_W, SCREEN_H);
 	if (!display)
 	{
 		std::cerr << "Display failed!" << std::endl;
-		return 0;
+		al_destroy_timer(timer);
+		return -1;
 	}
 
-	event_queue = al_create_event_queue();
+	bouncer = al_create_bitmap(BOUNCER_SIZE, BOUNCER_SIZE);
+	if (!bouncer)
+	{
+		std::cout << "Bouncer failed?" << std::endl;
+		al_destroy_display(display);
+		al_destroy_timer(timer);
+		return -1;
+	}
 
+	al_set_target_bitmap(bouncer);
+	al_clear_to_color(al_map_rgb(255, 0, 255));
+	al_set_target_bitmap(al_get_backbuffer(display));
+
+	event_queue = al_create_event_queue();
 	if (!event_queue)
 	{
 		std::cerr << "event Queue failed!" << std::endl;
+		al_destroy_bitmap(bouncer);
 		al_destroy_display(display);
-		return 0;
+		al_destroy_timer(timer);
+		return -1;
 	}
 
-	al_set_app_name("Azzura!");
-
 	al_register_event_source(event_queue, al_get_display_event_source(display));
+
+	al_register_event_source(event_queue, al_get_timer_event_source(timer));
 
 	al_clear_to_color(al_map_rgb(0, 0, 0));
 
 	al_flip_display();
 
+	al_start_timer(timer);
+
+	int r = 1;
+	int g = 1;
+	int b = 1;
+
 	while (1)
 	{
 		ALLEGRO_EVENT ev;
-		ALLEGRO_TIMEOUT timeout;
-		al_init_timeout(&timeout, 0.06);
+		al_wait_for_event(event_queue, &ev);
 
-		bool get_event = al_wait_for_event_until(event_queue, &ev, &timeout);
+		if (ev.type == ALLEGRO_EVENT_TIMER)
+		{
+			if (bouncer_x < 0 || bouncer_x > SCREEN_W - BOUNCER_SIZE)
+			{
+				bouncer_dx = -bouncer_dx;
+			}
 
-		if (get_event && ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE)
+			if (bouncer_y < 0 || bouncer_y > SCREEN_H - BOUNCER_SIZE)
+			{
+				bouncer_dy = -bouncer_dy;
+			}
+
+			bouncer_x += bouncer_dx;
+			bouncer_y += bouncer_dy;
+
+			redraw = true;
+		}
+		else if (ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE)
 		{
 			break;
 		}
 
-		al_clear_to_color(al_map_rgb(0, 0, 0));
-		al_flip_display();
+		if (redraw && al_is_event_queue_empty(event_queue))
+		{
+			redraw = false;
+			al_clear_to_color(al_map_rgb(r, g, b));
+			al_draw_bitmap(bouncer, bouncer_x, bouncer_y, 0);
+			al_flip_display();
+		}
 	}
 
+	al_destroy_bitmap(bouncer);
+	al_destroy_timer(timer);
 	al_destroy_display(display);
 	al_destroy_event_queue(event_queue);
 
