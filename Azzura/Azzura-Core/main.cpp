8b4541aca19c18fe7e1b7d258861cd114268a962
Will O <napalmhammer@gmail.com>
2016-02-23 00:07:53 -0500

    tested the bitmap and mouse events a bit, and included the rest of the project.

--- Azzura/Azzura-Core/main.cpp
+++ Azzura/Azzura-Core/main.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+/*#include <iostream>
 #include "allegro5\allegro5.h"
 
 const float FPS = 60;
@@ -16,7 +16,7 @@ int main(int argc, char** argv)
 
 	float bouncer_x = SCREEN_W / 2.0 - BOUNCER_SIZE / 2.0;
 	float bouncer_y = SCREEN_H / 2.0 - BOUNCER_SIZE / 2.0;
-	float bouncer_dx = -4.0, bouncer_dy = 4.0;
+	//float bouncer_dx = -4.0, bouncer_dy = 4.0;
 	bool redraw = true;
 
 	if(!al_init())	
@@ -25,11 +25,16 @@ int main(int argc, char** argv)
 		return -1;
 	}
 
+	if (!al_install_mouse())
+	{
+		std::cerr << "Mouse failed to initialize!" << std::endl;
+		return -1;
+	}
+
 	timer = al_create_timer(1.0 / FPS);
 	if (!timer)
 	{
 		std::cerr << "Failed to create timer!" << std::endl;
-		al_destroy_timer(timer);
 		return -1;
 	}
 
@@ -68,15 +73,17 @@ int main(int argc, char** argv)
 
 	al_register_event_source(event_queue, al_get_timer_event_source(timer));
 
+	al_register_event_source(event_queue, al_get_mouse_event_source());
+
 	al_clear_to_color(al_map_rgb(0, 0, 0));
 
 	al_flip_display();
 
 	al_start_timer(timer);
 
-	int r = 1;
-	int g = 1;
-	int b = 1;
+	int r = 255;
+	int g = 255;
+	int b = 255;
 
 	while (1)
 	{
@@ -85,22 +92,18 @@ int main(int argc, char** argv)
 
 		if (ev.type == ALLEGRO_EVENT_TIMER)
 		{
-			if (bouncer_x < 0 || bouncer_x > SCREEN_W - BOUNCER_SIZE)
-			{
-				bouncer_dx = -bouncer_dx;
-			}
-
-			if (bouncer_y < 0 || bouncer_y > SCREEN_H - BOUNCER_SIZE)
-			{
-				bouncer_dy = -bouncer_dy;
-			}
-
-			bouncer_x += bouncer_dx;
-			bouncer_y += bouncer_dy;
-
 			redraw = true;
 		}
 		else if (ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE)
+		{
+			break;
+		}
+		else if (ev.type == ALLEGRO_EVENT_MOUSE_AXES || ev.type == ALLEGRO_EVENT_MOUSE_ENTER_DISPLAY)
+		{
+			bouncer_x = ev.mouse.x;
+			bouncer_y = ev.mouse.y;
+		}
+		else if (ev.type == ALLEGRO_EVENT_MOUSE_BUTTON_UP)
 		{
 			break;
 		}
@@ -120,4 +123,65 @@ int main(int argc, char** argv)
 	al_destroy_event_queue(event_queue);
 
 	return 0;
-}
+}*/
+
+/*#include <iostream>
+#include "allegro5\allegro.h"
+#include "allegro5\allegro_image.h"
+#include "allegro5\allegro_native_dialog.h"
+
+int main(int argc, char **argv)
+{
+	ALLEGRO_DISPLAY *display = NULL;
+	ALLEGRO_BITMAP *image = NULL;
+
+	if (!al_init())
+	{
+		al_show_native_message_box(display, "Error", "Error", "Failed to init allegro!", NULL, ALLEGRO_MESSAGEBOX_ERROR);
+		return 0;
+	}
+
+	if (!al_init_image_addon())
+	{
+		al_show_native_message_box(display, "Error", "Error", "Failed to init Image Addon!", NULL, ALLEGRO_MESSAGEBOX_ERROR);
+		return 0;
+	}
+
+	display = al_create_display(800, 600);
+
+	if (!display)
+	{
+		al_show_native_message_box(display, "Error", "Error", "Failed to init Display!", NULL, ALLEGRO_MESSAGEBOX_ERROR);
+		return 0;
+	}
+
+	image = al_load_bitmap("../Dependencies/bmp/tank.bmp");
+	
+	if (!image)
+	{
+		al_show_native_message_box(display, "Error", "Error", "Failed to load Image!", NULL, ALLEGRO_MESSAGEBOX_ERROR);
+		al_destroy_display(display);
+		return 0;
+	}
+
+	al_draw_bitmap(image, 100, 100, 0);
+
+	image = al_load_bitmap("../Dependencies/bmp/tiger.png");
+
+	if (!image)
+	{
+		al_show_native_message_box(display, "Error", "Error", "Failed to load Image!", NULL, ALLEGRO_MESSAGEBOX_ERROR);
+		al_destroy_display(display);
+		return 0;
+	}
+
+	al_draw_bitmap(image, 350, 350, 0);
+
+	al_flip_display();
+	al_rest(5);
+
+	al_destroy_display(display);
+	al_destroy_bitmap(image);
+
+	return 0;
+}*/
